9f907f398a2362221dd4a7ca01834e6dd6f3d5f8
Tomeu Bestard Puig <tomeubestardpui@gmail.com>
2025-09-22 08:57:23 +0200

    fix: Implementa script de arranque definitivo para resolver el problema de entorno

--- build.sh
+++ build.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
-# 1. Crea explícitamente un entorno virtual
 python3 -m venv .venv
-
-# 2. Activa el entorno virtual. ¡Este es el paso clave que faltaba!
 source .venv/bin/activate
-
-# 3. Actualiza pip e instala las librerías DENTRO del entorno activado
 pip install --upgrade pip
 pip install -r requirements.txt
+
+# ¡NUEVA LÍNEA IMPORTANTE! Le da permiso de ejecución al script de arranque.
+chmod +x ./start.sh
